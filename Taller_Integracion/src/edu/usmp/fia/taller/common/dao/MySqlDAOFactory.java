57704e6eb748aafbbda32419edf3353cba58f45f
ragonzales <rogger.tgonzales@gmail.com>
2015-10-25 22:55:40 -0500

    simulacion

--- Taller_Integracion/src/edu/usmp/fia/taller/common/dao/MySqlDAOFactory.java
+++ Taller_Integracion/src/edu/usmp/fia/taller/common/dao/MySqlDAOFactory.java
@@ -176,10 +176,6 @@ public class MySqlDAOFactory extends DAOFactory {
 		return new MysqlFactoryConvalidacion();
 	}
 
-	
-
-
-
 	public DAOFactoryMCurricular getMallaCurricular() { return new MySqlFactoryMCurricular(); }
 
 
